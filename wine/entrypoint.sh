e67d6885308c1c2e218e05dfec6ac92102f0ea66
Kuma3mccm <55430208+Kuma3mccm@users.noreply.github.com>
2025-08-04 06:51:31 +0000

    コンソールが正常に表示されない問題の修正

--- wine/entrypoint.sh
+++ wine/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 cd /home/container
 
-# Information output
+# === 環境変数と端末設定 ===
+export TZ=Asia/Tokyo
+export LANG=ja_JP.UTF-8
+export LANGUAGE=ja_JP:ja
+export TERM=xterm-256color
+export COLUMNS=120
+export LINES=30
+
+# stty で端末幅を強制設定（折り返し防止）
+stty cols 120 rows 30
+
+# === 情報表示 ===
 echo "Running on Debian $(cat /etc/debian_version)"
 echo "Current timezone: $(cat /etc/timezone)"
 wine --version
@@ -10,7 +21,7 @@ wine --version
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-## just in case someone removed the defaults.
+# === Steam user設定 ===
 if [ "${STEAM_USER}" == "" ]; then
     echo -e "steam user is not set.\n"
     echo -e "Using anonymous user.\n"
@@ -21,66 +32,65 @@ else
     echo -e "user set to ${STEAM_USER}"
 fi
 
-## if auto_update is not set or to 1 update
+# === 自動アップデート処理 ===
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
-    # Update Source Server
     if [ ! -z ${SRCDS_APPID} ]; then
-	./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update 1007 +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+	    ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update 1007 +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
     else
         echo -e "No appid set. Starting Server"
     fi
-
 else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
+# === XVFB起動 ===
 if [[ $XVFB == 1 ]]; then
-        Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
+    Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
 fi
 
-# Install necessary to run packages
+# 初回エラーメッセージについて案内
 echo "First launch will throw some errors. Ignore them"
 
 mkdir -p $WINEPREFIX
 
-# Check if wine-gecko required and install it if so
+# === Geckoのインストール ===
 if [[ $WINETRICKS_RUN =~ gecko ]]; then
-        echo "Installing Gecko"
-        WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
+    echo "Installing Gecko"
+    WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
 
-        if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
-        fi
+    if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
+        wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
+    fi
 
-        if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
-        fi
+    if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
+        wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
+    fi
 
-        wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
-        wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
+    wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
+    wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
 fi
 
-# Check if wine-mono required and install it if so
+# === Monoのインストール ===
 if [[ $WINETRICKS_RUN =~ mono ]]; then
-        echo "Installing mono"
-        WINETRICKS_RUN=${WINETRICKS_RUN/mono}
+    echo "Installing mono"
+    WINETRICKS_RUN=${WINETRICKS_RUN/mono}
 
-        if [ ! -f "$WINEPREFIX/mono.msi" ]; then
-                wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.1.0/wine-mono-9.1.0-x86.msi
-        fi
+    if [ ! -f "$WINEPREFIX/mono.msi" ]; then
+        wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.1.0/wine-mono-9.1.0-x86.msi
+    fi
 
-        wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+    wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
 fi
 
-# List and install other packages
+# === Winetricksパッケージのインストール ===
 for trick in $WINETRICKS_RUN; do
-        echo "Installing $trick"
-        winetricks -q $trick
+    echo "Installing $trick"
+    winetricks -q $trick
 done
 
-# Replace Startup Variables
+# === Startupコマンド変数展開 ===
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
-eval ${MODIFIED_STARTUP}
+# === 仮想端末経由で実行（表示崩れ回避）===
+exec script -q -c "${MODIFIED_STARTUP}" /dev/null
