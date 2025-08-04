dab5e542987087ee58dd14253ddd202a41c6a4c9
Kuma3mccm <55430208+Kuma3mccm@users.noreply.github.com>
2025-08-04 07:37:27 +0000

    コンソールが正常に表示されない問題の修正

--- wine/entrypoint.sh
+++ wine/entrypoint.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 cd /home/container
 
-# === 環境変数と端末設定 ===
+# ターミナル・環境変数設定
 export TZ=Asia/Tokyo
 export LANG=ja_JP.UTF-8
 export LANGUAGE=ja_JP:ja
+export LC_ALL=ja_JP.UTF-8
 export TERM=xterm-256color
 export COLUMNS=120
 export LINES=30
 stty cols 120 rows 30
 
-# === 情報表示 ===
+# 情報出力
 echo "Running on Debian $(cat /etc/debian_version)"
 echo "Current timezone: $(cat /etc/timezone)"
 echo "Now: $(date)"
 wine --version
 
-# === 内部IP設定 ===
+# Docker内部IP取得
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# === Steamユーザー設定 ===
+# Steam認証処理
 if [ "${STEAM_USER}" == "" ]; then
     echo -e "steam user is not set.\n"
     echo -e "Using anonymous user.\n"
@@ -31,7 +32,7 @@ else
     echo -e "user set to ${STEAM_USER}"
 fi
 
-# === 自動アップデート処理 ===
+# 自動アップデート
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
     if [ ! -z ${SRCDS_APPID} ]; then
         ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update 1007 +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
@@ -42,56 +43,45 @@ else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
-# === XVFB 起動 ===
+# XVFB仮想ディスプレイ
 if [[ $XVFB == 1 ]]; then
     Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
 fi
 
 echo "First launch will throw some errors. Ignore them"
 
-# === WINEPREFIX 初期化 ===
+# WINEPREFIX 初期化
 mkdir -p $WINEPREFIX
 
-# === Gecko インストール ===
+# Gecko/MONO/winetricks
 if [[ $WINETRICKS_RUN =~ gecko ]]; then
     echo "Installing Gecko"
     WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
-    if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
-        wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
-    fi
-    if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
-        wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
-    fi
-    wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
-    wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
+    wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
+    wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
+    wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart
+    wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart
 fi
 
-# === Mono インストール ===
 if [[ $WINETRICKS_RUN =~ mono ]]; then
     echo "Installing mono"
     WINETRICKS_RUN=${WINETRICKS_RUN/mono}
-    if [ ! -f "$WINEPREFIX/mono.msi" ]; then
-        wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.1.0/wine-mono-9.1.0-x86.msi
-    fi
-    wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+    wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/9.1.0/wine-mono-9.1.0-x86.msi
+    wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart
 fi
 
-# === その他の Winetricks パッケージ ===
 for trick in $WINETRICKS_RUN; do
     echo "Installing $trick"
     winetricks -q $trick
 done
 
-# === STARTUPコマンド変数展開 ===
+# スタートアップコマンド展開
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# === ログ出力ファイルを仮想端末で実行し追跡 ===
-LOGFILE="wine_output.log"
-rm -f "$LOGFILE"
+# 出力を即時反映するために32bit stdbuf.soを使用
+export LD_PRELOAD="/opt/coreutils32/libexec/coreutils/libstdbuf.so"
+export STDBUF_PTS=1
 
-# 仮想端末上で wine を起動し、出力をすべてファイルへ
-script -q -c "${MODIFIED_STARTUP}" "$LOGFILE" &
-
-# Pterodactyl コンソールにログを即時表示
-tail -n 100 -F "$LOGFILE"
+# 実行（stderr含めて即時コンソールに反映）
+stdbuf -oL -eL bash -c "${MODIFIED_STARTUP}"
